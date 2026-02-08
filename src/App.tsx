ab08031ed531e7e392a920fb8260ee08eac60e62
Fabriziochiappini <seofibra@gmail.com>
2026-02-08 02:19:41 +0100

    Fix: Switch to BrowserRouter to support standard URLs on Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { DataProvider } from './context/DataContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
